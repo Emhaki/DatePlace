0003aea05761b9fbaa5043510290c349461c6a6e
emhaki <mhmh779@naver.com>
2024-10-13 14:08:02 +0900

    데이트 장소 추천 생성 페이지

--- date_recommender/recommender/forms.py
+++ date_recommender/recommender/forms.py
@@ -6,6 +6,15 @@ class DatePlaceForm(forms.ModelForm):
         model = DatePlace
         fields = ['name', 'category', 'address', 'description', 'rating', 'latitude', 'longitude', 'region']
         widgets = {
-            'description': forms.Textarea(attrs={'rows': 4}),
+            'name': forms.TextInput(attrs={'placeholder': '장소 이름을 입력하세요'}),
+            'address': forms.TextInput(attrs={'placeholder': '주소를 입력하세요'}),
+            'description': forms.Textarea(attrs={'rows': 4, 'placeholder': '장소에 대한 설명을 입력하세요'}),
+            'rating': forms.NumberInput(attrs={'min': 0, 'max': 5, 'step': 0.1}),
+            'latitude': forms.NumberInput(attrs={'readonly': 'readonly'}),
+            'longitude': forms.NumberInput(attrs={'readonly': 'readonly'}),
         }
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['region'].widget.attrs.update({'class': 'form-select'})
+        self.fields['category'].widget.attrs.update({'class': 'form-select'})
